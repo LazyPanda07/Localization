1c5cd9a45444fa73d3954ff9ceaadbbe144862b3
LazyPanda07 <semengricenko@gmail.com>
2021-11-08 00:08:11 +0500

    Added TextLocalization

---
+++ src/TextLocalization.cpp
@@ -0,0 +1,74 @@
+#include "TextLocalization.h"
+
+#include <stdexcept>
+#include <format>
+
+using namespace std;
+
+namespace localization
+{
+	TextLocalization::TextLocalization()
+	{
+		handle = LoadLibraryA("Localization.dll");
+
+		if (!handle)
+		{
+			throw runtime_error("Can't find Localization.dll");
+		}
+
+		dictionaries = reinterpret_cast<const unordered_map<string, const unordered_map<string, string>*>*>(GetProcAddress(handle, "dictionaries"));
+
+		if (!dictionaries)
+		{
+			throw runtime_error("Can't find dictionaries in Localization.dll, rebuild and try again");
+		}
+
+		originalLanguage = reinterpret_cast<const string*>(GetProcAddress(handle, "originalLanguage"));
+
+		if (!originalLanguage)
+		{
+			throw runtime_error("Can't find originalLanguage in Localization.dll, rebuild and try again");
+		}
+	}
+
+	TextLocalization::~TextLocalization()
+	{
+		FreeLibrary(handle);
+	}
+
+	TextLocalization& TextLocalization::get()
+	{
+		static TextLocalization instance;
+
+		return instance;
+	}
+
+	void TextLocalization::changeLanguage(const string& language)
+	{
+		if (dictionaries->find(language) == dictionaries->end())
+		{
+			throw runtime_error(format(R"(Wrong language value "{}")"sv, language));
+		}
+
+		this->language = language;
+	}
+
+	const string& TextLocalization::operator[] (const string& key) const
+	{
+		try
+		{
+			return dictionaries->at(language)->at(key);
+		}
+		catch (const out_of_range&)
+		{
+			try
+			{
+				return dictionaries->at(*originalLanguage)->at(key);
+			}
+			catch (const out_of_range&)
+			{
+				throw runtime_error(format(R"(Can't find key "{}")"sv, key));
+			}
+		}
+	}
+}
---
+++ src/TextLocalization.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+#include <Windows.h>
+
+namespace localization
+{
+	class TextLocalization
+	{
+	private:
+		const std::unordered_map<std::string, const std::unordered_map<std::string, std::string>*>* dictionaries;
+		const std::string* originalLanguage;
+		std::string language;
+		HMODULE handle;
+
+	private:
+		TextLocalization();
+
+		~TextLocalization();
+
+	public:
+		static TextLocalization& get();
+
+		/// @brief 
+		/// @param language 
+		/// @exception std::runtime_error
+		void changeLanguage(const std::string& language);
+
+		/// @brief 
+		/// @param key 
+		/// @return
+		/// @exception std::runtime_error 
+		const std::string& operator [] (const std::string& key) const;
+	};
+}
