b97c6b7d634421957bd3d53f1758de05a69e2f50
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-03-16 13:54:36 +0500

    Fix

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -19,7 +19,7 @@ TEST(Localization, MultiLocalizationManager)
 {
 	localization::MultiLocalizationManager& manager = localization::MultiLocalizationManager::getManager();
 
-	ASSERT_EQ(manager.getLocalizedString("LocalizationData", "first"), "First");
+	ASSERT_EQ(manager.getLocalizedString("LocalizationData", "first", "en"), "First");
 	ASSERT_EQ(manager.getLocalizedString("LocalizationData", "second", "en"), "Second");
 
 	ASSERT_EQ(manager.getLocalizedString("LocalizationData", "first", "ru"), json::utility::toUTF8JSON("Первый", CP_WINDOWS_1251));
