c73e8badb562bc5232590b998e982e3c4ab498f4
LazyPanda07 <semengricenko@gmail.com>
2025-06-10 23:48:57 +0500

    Add ARM build

--- Tests/tests.py
+++ Tests/tests.py
@@ -2,6 +2,25 @@ import platform
 import subprocess
 import json
 import sys
+import os
+from typing import List
+
+arm = os.getenv("MARCH", "")
+
+
+def run_process(args: List[str], working_dir: str) -> int:
+    global arm
+
+    if arm == "armv8-a":
+        temp_args: List = ["qemu-aarch64"]
+
+        for arg in args:
+            temp_args.append(arg)
+
+        return subprocess.run(args=temp_args, cwd=working_dir).returncode
+    else:
+        return subprocess.run(args=args, cwd=working_dir).returncode
+
 
 if __name__ == '__main__':
     executable_path = "../assets/"
@@ -9,10 +28,12 @@ if __name__ == '__main__':
 
     if platform.system() == "Windows":
         executable_path += "Windows/LocalizationUtils.exe"
+    elif arm == "armv8-a":
+        executable_path += "LinuxARM/LocalizationUtils"
     else:
         executable_path += "Linux/LocalizationUtils"
 
-    subprocess.run([executable_path, ".", "generate"], cwd=working_dir)
+    run_process([executable_path, ".", "generate"], working_dir)
 
     with open(f"{working_dir}/localization_utils_settings.json", "r") as file:
         settings = json.load(file)
@@ -22,7 +43,7 @@ if __name__ == '__main__':
     with open(f"{working_dir}/localization_utils_settings.json", "w") as file:
         file.write(json.dumps(settings))
 
-    subprocess.run([executable_path, ".", "generate"], cwd=working_dir)
+    run_process([executable_path, ".", "generate"], working_dir)
 
     en = {
         "first": "First",
@@ -32,7 +53,7 @@ if __name__ == '__main__':
     with open(f"{working_dir}/localization/localization_en.json", "w") as file:
         file.write(json.dumps(en))
 
-    subprocess.run([executable_path, ".", "generate"], cwd=working_dir)
+    run_process([executable_path, ".", "generate"], working_dir)
 
     with open(f"{working_dir}/localization/localization_ru.json", "r") as file:
         ru = json.load(file)
@@ -43,9 +64,9 @@ if __name__ == '__main__':
     with open(f"{working_dir}/localization/localization_ru.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(ru, ensure_ascii=False))
 
-    subprocess.run([executable_path, ".", "generate"], cwd=working_dir)
+    run_process([executable_path, ".", "generate"], working_dir)
 
     if sys.argv[1] == "Release":
-        subprocess.run([executable_path, ".", "release_build", "../Tests/build/bin"], cwd=working_dir)
+        run_process([executable_path, ".", "release_build", "../Tests/build/bin"], working_dir)
     else:
-        subprocess.run([executable_path, ".", "debug_build", "../Tests/build/bin"], cwd=working_dir)
+        run_process([executable_path, ".", "debug_build", "../Tests/build/bin"], working_dir)
